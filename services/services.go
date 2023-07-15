01e1ff02115e3f38b2e7085c3018264425b98e92
Dilshodjon <torex.amaki@gmail.com>
2023-07-15 20:41:34 +0500

    first commit

--- services/services.go
+++ services/services.go
@@ -9,6 +9,10 @@ import (
 
 func StartServices() {
 	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Next()
+	})
 	r.POST("/auth/login", auth.Login)
 	r.GET("/auth/getdata", auth.GetUsers)
 	err := r.Run()
